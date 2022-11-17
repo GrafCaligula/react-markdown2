d7535d3bfe1c41f9b75567a4354295825f4bc81a
Markus <markus.kloepper@gmx.net>
2022-11-17 11:33:03 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import data from "./data"
 
 export default function App() {
+  const [text, setText] = useState(data);
+
+  
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <h1>App Component</h1>
+    <div className="app">
+      <textarea id="editor" onChange={handleChange} value={text}></textarea>
+      <div id="preview" >
+      <ReactMarkdown>{text}</ReactMarkdown>
+      </div>
     </div>
   );
 }
